504d82f6bebcff10af0cb7cca582f5344382154d
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2026-05-17 02:08:00 +0530

    Fix Email Manager CI lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,2 +1,13 @@
-import config from "@saas-maker/eslint-config/next";
-export default config;
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+
+const compat = new FlatCompat({
+  recommendedConfig: js.configs.recommended,
+});
+
+export default [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    ignores: [".next/**", ".open-next/**", ".wrangler/**", "next-env.d.ts"],
+  },
+];
